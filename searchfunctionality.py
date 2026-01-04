e36c66f77e5db152d7a777c11be11c9f05eab71e
NurulZayanah <aw240019@student.uthm.edu.my>
2026-01-05 03:26:41 +0800

    searchfunctionality.py
    add tkinter 
    -not fully done yet

--- searchfunctionality.py
+++ searchfunctionality.py
@@ -1,9 +1,16 @@
-def search_book(self , title , book_list):
-    title = title.strip().lower()
-    found = False 
-    # Traverse linked list to find matches
+import tkinter as tk 
+from tkinter import simpledialog
+def search_book(self):
+    # input book
+    title = simpledialog.askstring("Search Book","Enter Title or Author:")
+    if not title:
+        return
+    title = title.strip().lower
+    results = []
+    # Find matches
     for book in self.book_list.to_list():
         if title in book["Title"].lower() or title in book["Author"].lower():
+            results.append(book)
             print(
                 book["Book_ID"], "|", book["Title"], "|", book["Author"], "|",
                 book["Year"], "|", book["Genre"], "|", "Available:", book["Available"]
@@ -12,3 +19,4 @@ def search_book(self , title , book_list):
 
     if not found:
         print("No matching books found.")
+
